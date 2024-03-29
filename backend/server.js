c568d2143d112042fb47e41b06019e0f5db70beb
sublymus <sublymus@gmail.com>
2024-03-29 13:15:58 +0300

    m

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
 });
 
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`serve at http://localhost:${port}`);
 });
